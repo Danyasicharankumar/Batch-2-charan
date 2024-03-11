7ddfdef3378afb7352e53c3b8859ac9b9dfef155
Danyasicharankumar <162984466+Danyasicharankumar@users.noreply.github.com>
2024-03-11 16:23:44 +0530

    Add files via upload

---
+++ cherry 04.py
@@ -0,0 +1,22 @@
+42#---> print(object, sep, end, file, flush)
+43 # print has 5 parameters
+44
+45 # object-->the value to be printed is object
+46 # sep--> seperator
+47 # Eg
+48 #a8
+49 #b 9
+50 #c 89
+51 # print(a, b,c, sep='$')
+52
+53 # end --> which is used to add the string at the end of the value,
+54 ## It will not keep the command to stay in same line
+55 # Eg:
+56 # print(str(a), b, end='%%')
+57 # print(str(c))
+58#1.) Variable have to start with only alphabets, underscores
+#2.) The char following the 1st char can be alphabets, numbers, underscores
+#3.) No spaces are allowed
+# 4.) No special characters like !@#$%^&*()+
+#5.) case sensitive
+#6.) Keywords are not allowed as variables
